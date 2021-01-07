49a2cf37ff401ec188866d04e60c7709d29c0149
haluskua <haluskua.one@gmail.com>
2021-01-08 09:58:39 +1300

    added header and HOC file with Components folder

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Resources/css/app.css";
+
+import Routes from "./routes";
+import { BrowserRouter } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Layout from './Hoc/Layout';
+const Routes = (props) => {
+  return (
+    <Layout>second line</Layout>
+  )
+};
+
+export default Routes;
