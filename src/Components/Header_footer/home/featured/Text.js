532c347d6b7f4caa22cb35e82f0c805f44fdd666
haluskua <haluskua.one@gmail.com>
2021-01-19 10:01:37 +1300

    saving front-page absolute properties for containers

--- src/Components/Header_footer/home/featured/Text.js
+++ src/Components/Header_footer/home/featured/Text.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { easePolyOut } from "d3-ease";
 import Animate from "react-move/Animate";
 
-import FeaturedPlayer from '../../../../Resources/images/hero.png'
+import FeaturedLogo from "../../../../Resources/images/logos/logo.png";
 class Text extends Component {
   animateNumber = () => (
     <Animate
@@ -32,109 +32,135 @@ class Text extends Component {
       }}
     </Animate>
   );
-  
+
   animateFirst = () => (
     <Animate
-    show={true}
-    start={{
-      opacity: 0,
-      x:503,
-      y:450
-    }}
-    enter={{
-      opacity: [1],
-      x:[273],
-      y:[450],
-      timing: { duration: 500, ease: easePolyOut },
-    }}
-  >
-    {({ opacity, x, y}) => {
-      return (
-        <div
-          className="featured_first"
-          style={{
-            opacity,
-            transform: `translate(${x}px,${y}px)`,
-          }}
-        >
-          My
-        </div>
-      );
-    }}
-  </Animate>
-  )
-  
+      show={true}
+      start={{
+        opacity: 0,
+        x: 503,
+        y: 450,
+      }}
+      enter={{
+        opacity: [1],
+        x: [273],
+        y: [450],
+        timing: { duration: 500, ease: easePolyOut },
+      }}
+    >
+      {({ opacity, x, y }) => {
+        return (
+          <div
+            className="featured_first"
+            style={{
+              opacity,
+              transform: `translate(${x}px,${y}px)`,
+            }}
+          >
+            My
+          </div>
+        );
+      }}
+    </Animate>
+  );
+
   animateSecond = () => (
     <Animate
-    show={true}
-    start={{
-      opacity: 0,
-      x:503,
-      y:586
-    }}
-    enter={{
-      opacity: [1],
-      x:[273],
-      y:[586],
-      timing: {delay: 300, duration: 500, ease: easePolyOut },
-    }}
-  >
-    {({ opacity, x, y}) => {
-      return (
-        <div
-          className="featured_second"
-          style={{
-            opacity,
-            transform: `translate(${x}px,${y}px)`,
-          }}
-        >
-          Portfolio
-        </div>
-      );
-    }}
-  </Animate>
-      
-  )
-  
-  animatePlayer = () => (
+      show={true}
+      start={{
+        opacity: 0,
+        x: 503,
+        y: 586,
+      }}
+      enter={{
+        opacity: [1],
+        x: [273],
+        y: [586],
+        timing: { delay: 300, duration: 500, ease: easePolyOut },
+      }}
+    >
+      {({ opacity, x, y }) => {
+        return (
+          <div
+            className="featured_second"
+            style={{
+              opacity,
+              transform: `translate(${x}px,${y}px)`,
+            }}
+          >
+            Portfolio
+          </div>
+        );
+      }}
+    </Animate>
+  );
+
+  animateLogo = () => (
     <Animate
-    show={true}
-    start={{
-      opacity: 0,
+      show={true}
+      start={{
+        opacity: 0,
+      }}
+      enter={{
+        opacity: [1],
 
-    }}
-    enter={{
-      opacity: [1],
+        timing: { delay: 800, duration: 500, ease: easePolyOut },
+      }}
+    >
+      {({ opacity, x, y }) => {
+        return (
+          <div
+            className="featured_logo"
+            style={{
+              opacity,
+              background: `url(${FeaturedLogo})`,
+              transform: `translate(550px,201px)`,
+            }}
+          ></div>
+        );
+      }}
+    </Animate>
+  );
 
-      timing: {delay: 800, duration: 500, ease: easePolyOut },
-    }}
-  >
-    {({ opacity, x, y}) => {
-      return (
-        <div
-          className="featured_player"
-          style={{
-            opacity,
-            background: `url(${FeaturedPlayer})`,
-            transform: `translate(550px,201px)`,
-          }}
-        >
-        </div>
-      );
-    }}
-  </Animate> 
-      
-  )
+  animateTitle = () => (
+    <Animate
+      show={true}
+      start={{
+        opacity: 0,
+        rotate: 0,
+      }}
+      enter={{
+        opacity: [1],
+        rotate: [360],
+        timing: { delay: 1100, duration: 500, ease: easePolyOut },
+      }}
+    >
+      {({ opacity, rotate }) => {
+        return (
+          <div
+            className="title"
+            style={{
+              opacity,
+              transform: `translate(260px, 170px) rotateY(${rotate}deg),`,
+            }}
+          >
+            FullStack Developer
+          </div>
+        );
+      }}
+    </Animate>
+  );
 
   render() {
-    return <div className="featured_text">
-    {this.animateNumber()}
-    {this.animateFirst()}
-    {this.animateSecond()}
-    {this.animatePlayer()}
-    
-    
-    </div>;
+    return (
+      <div className="featured_text">
+        {this.animateNumber()}
+        {this.animateTitle()}
+        {this.animateFirst()}
+        {this.animateSecond()}
+        {this.animateLogo()}
+      </div>
+    );
   }
 }
 
