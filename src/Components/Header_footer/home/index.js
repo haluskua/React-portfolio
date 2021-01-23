dc5c3ee74f7cb3fede559b12f7ce6026ca16cbae
haluskua <haluskua.one@gmail.com>
2021-01-24 11:37:40 +1300

    added Meed Players component

--- src/Components/Header_footer/home/index.js
+++ src/Components/Header_footer/home/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Featured from "./featured";
 import MatchesHome from "./matches/index";
+import MeetPlayers from './meetPlayers';
 
 const Home = () => {
   return (
     <div className="bck_blue_featured">
       <Featured />
       <MatchesHome />
+      <MeetPlayers />
     </div>
   );
 };
--- src/Components/Header_footer/index.js
+++ src/Components/Header_footer/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Featured from './home/featured';
+
+
+
 const Home = () => {
     return (
         <div className="bck_blue">
