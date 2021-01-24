e9b9d1a83004d29ec947d7804abe542f4a5bd390
haluskua <haluskua.one@gmail.com>
2021-01-25 10:22:58 +1300

    added player jersey and finishing player card component

---
+++ src/Components/Header_footer/home/promotion/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PromotionAnimation from './Animation';
+
+const Promotion = () => {
+    return (
+        <div className="promotion_wrapper"
+        
+         style={{
+             background: '#ffffff'
+         }}
+        >
+        
+        <div className="containter">
+                <PromotionAnimation />
+        </div>
+            
+        </div>
+    );
+};
+
+export default Promotion;
